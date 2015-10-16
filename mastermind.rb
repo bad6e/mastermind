a3742b6715a9cf105b638c618888942d91a6e16a
Bret Doucette <doucette.bret@gmail.com>
2015-10-16 10:55:41 -0600

    ugh

--- mastermind.rb
+++ mastermind.rb
@@ -1,8 +1,34 @@
 require 'pry'
 
-puts "Welcome to Mastermind"
+def guess_checker(guess)
+  if guess == "c"
+    puts "Here is the secret code loser #{@final}"
+    exit!
+  elsif  guess.length < 4
+    puts "Code is too short - must be four letters"
+  elsif guess.length > 4
+    puts "Code is too long - must be four letter"
+  elsif guess == "q"
+    puts "exiting the game"
+  else
+    puts "Let's play!"
+  end
+end
 
+def randomizer
+  choices = ["r","g","b","y"]
+  shuffle_choices = choices.shuffle
+  @final = []
+
+  4.times do
+    @final << choices.sample
+  end
+end
+
+puts "Welcome to Mastermind"
 puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+
+randomizer
 
 instructions = gets.chomp.to_s
 
@@ -10,46 +36,34 @@ if instructions == "p"
   puts "I have generated a beginner sequence with four elements made up of: (r)ed,
         (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
         What's your guess?"
+
+
   guess = gets.chomp.to_s
-elsif instructions == "i"
-  puts "I don't have time to make an instructions file"
-else
-  puts "Exiting Program"
+
+
+  guess_checker(guess)
+
 end
 
-choices = ["r","g","b","y"]
-
-
-if guess == "c"
-  puts "Here is the secret code loser"
-  puts random_choices.to_s
-elsif  guess.length < 4
-  puts "Code is too short - must be four letters"
-elsif guess.length > 4
-  puts "Code is too long - must be four letter"
-elsif guess == "q"
-  puts "exiting the game"
-else
-  puts "Let's play!"
-end
 
 guess_array = guess.split("")
+difference = guess_array - @final
 
-difference = choices - guess_array
-#binding.pry
-
-results = guess_array.zip(choices).map {|x,y| x==y}
+results = guess_array.zip(@final).map {|x,y| x==y}
 
 counter = results.count do |word|
   word == true
 end
+guess_counter = 0
+guess_counter += 1
+
+puts guess_array.inspect
+puts @final.inspect
+
+puts "Guess: #{guess.to_s} has NOT DONE of the correct elements with #{counter} elements in the correct position(s)
+# You've taken #{guess_counter} guess"
 
 
-# puts guess.inspect
-# puts random_choices.inspect
-
-puts "Guess: #{guess.to_s} has #{4 - difference.count} of the correct elements with #{counter} elements in the correct position(s)
-# You've taken 1 guess"
 
 
 

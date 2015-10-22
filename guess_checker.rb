deeda96fce073621d44d3c08032c0d95f938fe4b
Bret Doucette <doucette.bret@gmail.com>
2015-10-22 09:43:32 -0600

    Added logic to check for guess

--- guess_checker.rb
+++ guess_checker.rb
@@ -1,8 +1,7 @@
 class GuessChecker
   def self.guess(guess)
     if guess == "c"
-      puts "Here is the secret code loser #{@final}"
-      exit!
+      puts "Here is the secret code loser"
     elsif  guess.length < 4
       puts "Code is too short - must be four letters"
     elsif guess.length > 4
